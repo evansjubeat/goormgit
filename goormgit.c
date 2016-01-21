fe1fd7096dc49a9e06ec7173cecccc65c338dd07
evansjubeat <jaehoon.moon@goorm.io>
2016-01-21 09:39:02 +0000

    this is icono, my new name

--- goormgit.c
+++ goormgit.c
@@ -4,6 +4,6 @@ int main()
 	printf("newgit from goorm!");
 	printf("let's see this can see past");
 	printf("This is goorgit");
-	printf("icono commit");
+	printf("icono commit it is");
 	return 0;
 }
